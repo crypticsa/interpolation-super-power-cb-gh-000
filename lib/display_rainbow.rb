0807f89ae9d855007309ad56c313e68fe5fcb024
Dusto´n Lomrt <simon.m.f.w.knott@eclipso.de>
2019-01-03 00:10:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
-def display_rainbow(colors)
-  puts colors.join(", ")
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+def display_rainbow(color_list)
+  puts "R: #{color_list[0]}, O: #{color_list[1]}, Y: #{color_list[2]}, G: #{color_list[3]}, B: #{color_list[4]}, I: #{color_list[5]}, V: #{color_list[6]}"
 end
